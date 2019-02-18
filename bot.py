73db85d4f9c52f3be79592eba6f31aacd1840c13
Charles Cheung <chklch@charless-mbp-2.lan>
2019-02-18 13:02:38 -0500

    Bot will now listen to mentions and respond with search term

--- bot.py
+++ bot.py
@@ -10,6 +10,20 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+@bot.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith(bot.user.mention):
+        # msg = 'Hello {0.author.mention}'.format(message)
+        #
+        msg = message.content.split(bot.user.mention)
+        if len(msg) == 2:
+            query = msg[1]
+            await message.channel.send("Will search for " + query)
+
 @bot.command()
 async def greet(ctx):
     await ctx.send(":smiley: :wave: Hello, there!")
