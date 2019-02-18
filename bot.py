7dbc95a39243e5cb7192a116445f4214ad5509de
Charles Cheung <chklch@charless-mbp-2.lan>
2019-02-18 16:27:02 -0500

    First finished pass of RomBot

--- bot.py
+++ bot.py
@@ -21,40 +21,59 @@ async def on_message(message):
     if message.content.startswith(bot.user.mention):
         # msg = 'Hello {0.author.mention}'.format(message)
         #
-        msg = message.content.split(bot.user.mention)
+        msg = message.content.split(bot.user.mention + " ")
         if len(msg) == 2:
             query = msg[1]
             url = "https://www.romexchange.com/api"
 
             params = {
-                'item': query,
-                'exact': "false"
+                "exact": "false",
+                "item": query,
             }
 
             response = requests.get(url=url, params=params)
-
             json_response = response.json()
+
+            if len(json_response) == 0:
+                await message.channel.send("Could not find {0}".format(query))
+                return
+
+
+            if len(json_response) > 6:
+                item_list = list(map(lambda item: item["name"], json_response))
+                item_list_filtered = list(filter(lambda item: item.find("[") == -1, item_list))
+                item_list_filtered_with_mention = list(map(lambda item: "{0} {1}".format(bot.user.mention, item), item_list_filtered))
+
+                item_string = '\n'.join(item_list_filtered_with_mention)
+
+                await message.channel.send("Too many results returned.  Try the following?\n\n{0}".format(item_string))
+                return
 
             for item in json_response:
                 item_name = item["name"]
                 item_image_url = item["image"]
-                item_global_price = item["global"]["latest"]
-                item_sea_price = item["sea"]["latest"]
+                item_global_price = '{:,.0f}'.format(item["global"]["latest"])
+                item_sea_price = '{:,.0f}'.format(item["sea"]["latest"])
 
-                if item_image_url is None:
-                    item_image_url = ""
+                field_message = "Global: {0}z\nSEA: {1}z".format(item_global_price, item_sea_price)
 
-                if item_global_price is None:
-                    item_global_price = "Unknown"
+                embedded_message = discord.Embed(color=0x1ef1bd)
+                embedded_message.title = item_name
+                embedded_message.description = field_message
 
-                if item_sea_price is None:
-                    item_sea_price = "Unknown"
+                if item_image_url is not None:
+                    embedded_message.set_image(url=item_image_url)
 
-                rom_message = "{0}, {1}, Global: {2} z, SEA: {3} z".format(item_name, item_image_url, item_global_price, item_sea_price)
-                await message.channel.send(rom_message)
+                await message.channel.send(embed=embedded_message)
+
 
 @bot.command()
 async def greet(ctx):
     await ctx.send(":smiley: :wave: Hello, there!")
+
+
+def item_name(item):
+    return item["name"]
+
 
 bot.run("NTQ3MDg3NTIxOTY4OTQ3MjEx.D0x8LA.FzOkJ9jjlK91PRS6LlNDj0JcPMM")
