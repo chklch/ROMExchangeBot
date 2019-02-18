137066348d4202b7c7c92f59dc0d736244aac68e
Charles Cheung <chklch@charless-mbp-2.lan>
2019-02-18 13:46:07 -0500

    Bot queries ROM and prints out names of items found

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import requests
+# import json
 
 bot = commands.Bot(command_prefix='$')
 
@@ -22,7 +24,20 @@ async def on_message(message):
         msg = message.content.split(bot.user.mention)
         if len(msg) == 2:
             query = msg[1]
-            await message.channel.send("Will search for " + query)
+            url = "https://www.romexchange.com/api"
+
+            params = {
+                'item': query,
+                'exact': "false"
+            }
+
+            response = requests.get(url=url, params=params)
+            # import pdb; pdb.set_trace()
+            json_response = response.json()
+
+            for item in json_response:
+                item_name = item["name"]
+                await message.channel.send(item_name)
 
 @bot.command()
 async def greet(ctx):
