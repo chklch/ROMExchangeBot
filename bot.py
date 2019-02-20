2e51cd0141018d2ee57718344bc77a17f62a6d81
Charles Cheung <chklch@charless-mbp-2.lan>
2019-02-19 19:15:42 -0500

    testing out prod logs

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    sys.stdout.flush()
 
 @bot.event
 async def on_message(message):
@@ -24,8 +25,6 @@ async def on_message(message):
         return
 
     if message.content.startswith(bot.user.mention):
-        # msg = 'Hello {0.author.mention}'.format(message)
-        #
         msg = message.content.split(bot.user.mention + " ")
         if len(msg) == 2:
             query = msg[1]
@@ -37,43 +36,49 @@ async def on_message(message):
 
             response = requests.get(url=rom_exchange_api, params=params)
             json_response = response.json()
-
-            if len(json_response) == 0:
-                await message.channel.send("Could not find '{0}'".format(query))
-                return
+            _send_response(query, message, json_response)
 
 
-            if len(json_response) > 6:
-                item_list = list(map(lambda item: item["name"], json_response))
-                item_list_filtered = list(filter(lambda item: item.find("[") == -1, item_list))
-                item_list_filtered_with_mention = list(map(lambda item: "{0} {1}".format(bot.user.mention, item), item_list_filtered))
+def _send_response(query, message, json_response):
+    if len(json_response) == 0:
+        await message.channel.send("Could not find '{0}'".format(query))
+        return
 
-                item_string = '\n'.join(item_list_filtered_with_mention)
+    if len(json_response) > 6:
+        item_list = list(map(lambda item: item["name"], json_response))
+        item_list_filtered = list(filter(lambda item: item.find("[") == -1, item_list))
+        item_list_filtered_with_mention = list(
+            map(lambda item: "{0} {1}".format(bot.user.mention, item), item_list_filtered))
 
-                await message.channel.send("Too many results returned.  Try the following?\n\n{0}".format(item_string))
-                return
+        item_string = '\n'.join(item_list_filtered_with_mention)
 
-            for item in json_response:
-                item_name = item["name"]
-                item_image_url = item["image"]
-                item_global_price = '{:,.0f}'.format(item["global"]["latest"])
-                item_global_week_change = _get_formatted_week_change(item["global"]["week"]["change"])
-                item_sea_price = '{:,.0f}'.format(item["sea"]["latest"])
-                item_sea_week_change = _get_formatted_week_change(item["sea"]["week"]["change"])
+        await message.channel.send("Too many results returned.  Try the following?\n\n{0}".format(item_string))
+        return
 
-                field_message = "Global: {0}z ({1} in 1 week)\nSEA: {2}z ({3} in 1 week)".format(item_global_price, item_global_week_change, item_sea_price, item_sea_week_change)
+    for item in json_response:
+        item_name = item["name"]
+        item_image_url = item["image"]
+        item_global_price = '{:,.0f}'.format(item["global"]["latest"])
+        item_global_week_change = _get_formatted_week_change(item["global"]["week"]["change"])
+        item_sea_price = '{:,.0f}'.format(item["sea"]["latest"])
+        item_sea_week_change = _get_formatted_week_change(item["sea"]["week"]["change"])
 
-                embedded_message = discord.Embed(color=0x1ef1bd)
-                embedded_message.title = item_name
-                embedded_message.description = field_message
+        field_message = "Global: {0}z ({1} in 1 week)\nSEA: {2}z ({3} in 1 week)".format(item_global_price,
+                                                                                         item_global_week_change,
+                                                                                         item_sea_price,
+                                                                                         item_sea_week_change)
 
-                encoded_item_name = quote(item_name)
-                embedded_message.url = rom_exchange_endpoint + "?q=" + encoded_item_name + "&exact=true"
+        embedded_message = discord.Embed(color=0x1ef1bd)
+        embedded_message.title = item_name
+        embedded_message.description = field_message
 
-                if item_image_url is not None:
-                    embedded_message.set_image(url=item_image_url)
+        encoded_item_name = quote(item_name)
+        embedded_message.url = rom_exchange_endpoint + "?q=" + encoded_item_name + "&exact=true"
 
-                await message.channel.send(embed=embedded_message)
+        if item_image_url is not None:
+            embedded_message.set_image(url=item_image_url)
+
+        await message.channel.send(embed=embedded_message)
 
 
 def _get_formatted_week_change(value):
